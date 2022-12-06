8fde942c51338dc906df0afaa4cc94eea29b7ea9
Ananda Badmaev <a.badmaev@clicknet.pro>
2022-12-06 15:30:10 +0300

    added config, readme and etc

--- gpio_service.py
+++ gpio_service.py
@@ -2,24 +2,22 @@
 
 from os import system
 import socket
+from sys import stderr
 from time import sleep
-from json import dumps
+from json import dumps, load
 
 def read_values(filename: str) -> list:
     file = open(filename)
     lines = file.readlines()
-    n = len(lines)
     val1 = int(lines[0])
     val2 = int(lines[1])
     file.close()
     return [val1, val2]
 
 
-def get_gpio_values() -> list | None:
-    gpio1 = " 1"
-    gpio2 = " 2"
+def get_gpio_values(port1: int, port2: int) -> list | None:
     script = "./get_gpio_values.sh"
-    ret = system(script + gpio1 + gpio2)
+    ret = system(script + " " + str(port1) + " " + str(port2))
     if ret == 0:
         return read_values("gpio_data")
     else:
@@ -36,20 +34,24 @@ def send_data(values: list, addr: str, port: int):
 
 
 def main_loop():
-    addr = "192.168.1.2"
-    port = 10000
+    conf_file = open('config.json')
+    conf = load(conf_file)
+    addr  = conf['server_addr']
+    port = conf['server_port']
+    update_period = conf['update_period_ms']
+    gpio1 = conf['GPIO_1']
+    gpio2 = conf['GPIO_2']
+
     gpio_data = [0, 0]
     while True:
-        new_data = get_gpio_values()
+        new_data = get_gpio_values(gpio1, gpio2)
         if new_data == None:
-            print("failed to obtain gpio values")
+            print("failed to obtain gpio values", file=stderr)
         else:
             if new_data != gpio_data:
                 send_data(new_data, addr, port)
                 gpio_data = new_data
-                print("data sent")
-        sleep(0.05)
-
+        sleep(update_period / 1000)
 
 main_loop()
 
